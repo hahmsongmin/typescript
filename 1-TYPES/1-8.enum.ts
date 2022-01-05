37ec5c38bd2f4993c511123d1ee1ba058587b2f0
songmin <tft0720@gmail.com>
2022-01-05 09:49:13 +0900

    enum쓰지 않고 union type으로 =>

--- 1-TYPES/1-8.enum.ts
+++ 1-TYPES/1-8.enum.ts
@@ -7,8 +7,26 @@
     const MONDAY = 0;
     const TUESDAY = 1;
     const WEDNESDAY = 2;
+    const THURSDAY = 3;
+    const FRIDAY = 4;
+    const SATURDAY = 5;
+    const SUNDAY = 6;
+
+    // JavaScript에서 사용하는 법
+    // =>
+    const DAYS_ENUM = Object.freeze({
+      MONDAY: 0,
+      TUESDAY: 1,
+      WEDNESDAY: 2,
+      THURSDAY: 3,
+      FRIDAY: 4,
+      SATURDAY: 5,
+      SUNDAY: 6,
+    });
 
     // TypeScript <== enum 잘 사용 하지 않음(Type보장이 안됨)
+    // 0부터 시작임,
+    // 0부터 시작하기 싫다면 숫자 지정해주면 됨 = 1
     enum Days {
       Monday = 'mon', // 0
       Tuesday = 'tue', // 1
@@ -23,8 +41,21 @@
 
     // 🌟 위처럼 잘 사용하지 않고 요렇게 사용
     type Dayss = 'Monday' | 'Tuesday' | 'Wednesday';
-    const day: Dayss = 'Wednesday';
+    const day: Dayss = 'Wednesday'; // ⭐️ 정확한 타입 보장
   }
   {
+    enum Days {
+      Monday,
+      Tuesday,
+      Wednesday,
+      Thursday,
+      Friday,
+      Saturday,
+      Sunday,
+    }
+    // TypeScript 에서 잘 사용하지 않는 이유
+    let day: Days = Days.Saturday;
+    day = Days.Monday;
+    day = 12; // <= 숫자 아무거나 할당 가능 // 컴파일시에도 에러 없음
   }
 }
